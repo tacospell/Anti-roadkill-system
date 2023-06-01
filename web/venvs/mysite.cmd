e751d07d09db58fb218c8c5b71d57726857b420d
tacospell <iceclaw0415@gmail.com>
2023-06-01 20:22:31 +0900

    New web Project

---
+++ web/venvs/mysite.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd c:/users/82103/downloads/anti-roadkill-system/anti-roadkill-system/web/projects/mysite
+c:/users/82103/downloads/anti-roadkill-system/anti-roadkill-system/web/venvs/mysite/scripts/activate
